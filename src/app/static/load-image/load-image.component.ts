3882c495721515897bfd901cde288328ff6428d2
Kunszt Norbert <kunszt.norbert@gmail.com>
2024-01-26 21:48:48 +0100

    modify design

--- src/app/static/load-image/load-image.component.ts
+++ src/app/static/load-image/load-image.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class LoadImageComponent implements OnInit {
 @Input() source:string = "";
+@Input() imageClassIfExist:string="";
 constructor(){
 
 }
