0e9fd83ab133549793b0032e05987d5c4fe14f86
Kunszt Norbert <kunszt.norbert@gmail.com>
2024-01-28 14:10:01 +0100

    embed images in tattoo component

--- src/app/static/services/tattoo/tattoo.component.ts
+++ src/app/static/services/tattoo/tattoo.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./tattoo.component.scss']
 })
 export class TattooComponent {
-
+  public slides = [
+    {src: "../../../assets/csillam/1.jpg" },
+    {src:"../../../assets/csillam/2.jpg"},
+    {src:"../../../assets/csillam/3.jpg"},
+    {src:"../../../assets/csillam/4.jpg"},
+    {src:"../../../assets/csillam/5.png"},
+  ];
 }
