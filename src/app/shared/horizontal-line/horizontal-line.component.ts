add64dd1295e59c91b00b49d78e8704ae7b8cac1
Kunszt Norbert <kunszt.norbert@gmail.com>
2024-01-29 16:15:29 +0100

    modify horizontal line selector

--- src/app/shared/horizontal-line/horizontal-line.component.ts
+++ src/app/shared/horizontal-line/horizontal-line.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-horizontal-line',
+  selector: 'horizontal-line',
   templateUrl: './horizontal-line.component.html',
   styleUrls: ['./horizontal-line.component.scss']
 })
