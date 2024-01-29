fed7a4f80095e723bc39af330d1ebc8f408f0606
Kunszt Norbert <kunszt.norbert@gmail.com>
2024-01-29 17:47:00 +0100

    use shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './static/home/home.component';
+
+
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoadImageComponent } from './shared/load-image/load-image.component';
+
+import { HomeComponent } from './static/home/home.component';
 import { MenuComponent } from './static/menu/menu.component';
 import { AboutComponent } from './static/about/about.component';
 import { MainComponent } from './static/main/main.component';
 import { ContactComponent } from './static/contact/contact.component';
-import { HorizontalLineComponent } from './shared/horizontal-line/horizontal-line.component';
 import { CarouselComponent } from './dinamic/carousel/carousel.component';
 import { ServicesComponent } from './static/services/services.component';
 import { KineziologiaComponent } from './static/services/kineziologia/kineziologia.component';
@@ -17,20 +18,16 @@ import { AccessComponent } from './static/services/access/access.component';
 import { JumpWithFrogsComponent } from './static/services/jump-with-frogs/jump-with-frogs.component';
 import { TattooComponent } from './static/services/tattoo/tattoo.component';
 import { PriceComponent } from './static/price/price.component';
-import { TitleComponent } from './shared/title/title.component';
 import { SharedModule } from './shared/shared.module';
-import { UpToTopComponent } from './shared/up-to-top/up-to-top.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    LoadImageComponent,
     MenuComponent,
     AboutComponent,
     MainComponent,
     ContactComponent,
-    HorizontalLineComponent,
     CarouselComponent,
     ServicesComponent,
     KineziologiaComponent,
@@ -38,15 +35,14 @@ import { UpToTopComponent } from './shared/up-to-top/up-to-top.component';
     JumpWithFrogsComponent,
     TattooComponent,
     PriceComponent,
-    TitleComponent,
-UpToTopComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,SharedModule,
+    AppRoutingModule,
+    SharedModule,
     NgbModule
   ],
   providers: [],
-  bootstrap: [MainComponent],exports:[TitleComponent]
+  bootstrap: [MainComponent]
 })
 export class AppModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HelloComponent } from './hello/hello.component';
+import { UpToTopComponent } from './up-to-top/up-to-top.component';
+import { TitleComponent } from './title/title.component';
+import { HorizontalLineComponent } from './horizontal-line/horizontal-line.component';
+import { LoadImageComponent } from './load-image/load-image.component';
 
 
 
 @NgModule({
   declarations: [
     HelloComponent,
+    UpToTopComponent,
+    TitleComponent,
+    HorizontalLineComponent,
+    LoadImageComponent
   ],
   imports: [
     CommonModule
   ],
-  exports:[]
+  exports:[
+    UpToTopComponent,
+    TitleComponent,
+    HorizontalLineComponent,
+    LoadImageComponent]
 })
 export class SharedModule { }
